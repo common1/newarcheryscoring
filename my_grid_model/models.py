729d12848070295e1e95fb775e797194ce1fd6ba
Harrie Smulders <h.l.w.smulders@tue.nl>
2026-01-26 16:05:17 +0100

    32 Working on my_grid_model - Part 1

--- my_grid_model/models.py
+++ my_grid_model/models.py
@@ -1,20 +1,8 @@
 from django.db import models
 
-class BaseCell(models.Model):
-    value = models.CharField(max_length=64)
-
-    previous_column = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
-    next_column = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
-    previous_row = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
-    next_row = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
-
-    class Meta:
-        abstract = True
-
 class SimpleGrid(models.Model):
     name = models.CharField(max_length=128)
     data = models.JSONField(null=True)
-        
+
     def __str__(self):
         return self.name
-    
